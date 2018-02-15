cb48c8de6dc111526995d1a82b85dea07a992730
Zac-L <Zachlichens@gmail.com>
2018-02-15 13:35:20 -0800

    parcel build files

--- dist/reddit-search-app.js
+++ dist/reddit-search-app.js
@@ -69,10 +69,89 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({4:[function(require,module,exports) {
+})({3:[function(require,module,exports) {
+"use strict";
 
-console.log('123'); // eslint-disable-line
-},{}],6:[function(require,module,exports) {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = {
+  search: (searchTerm, searchLimit, sortBy) => {
+    return fetch(`http://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`).then(res => res.json()).then(data => data.data.children.map(data => data.data)).catch(error => console.log(error));
+  }
+};
+},{}],2:[function(require,module,exports) {
+'use strict';
+
+var _reddit = require('./reddit.api');
+
+var _reddit2 = _interopRequireDefault(_reddit);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+
+searchForm.addEventListener('submit', event => {
+  const searchTerm = searchInput.value;
+  const sortBy = document.querySelector('input[name="sortby"]:checked').value;
+  const searchLimit = document.getElementById('limit').value;
+
+  if (searchTerm === '') {
+    showMessage('Please add a search term', 'alert-danger');
+  }
+
+  // console.log(searchLimit);
+  searchInput.value = '';
+
+  _reddit2.default.search(searchTerm, searchLimit, sortBy).then(results => {
+    let output = '<div class="card-columns">';
+
+    results.forEach(post => {
+      const image = post.preview ? post.preview.images[0].source.url : 'https://cdn.comparitech.com/wp-content/uploads/2017/08/reddit-1.jpg';
+
+      output += `
+        <div class="card">
+          <img class="card-img-top" src="${image}" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">${post.title}</h5>
+            <p class="card-text">${truncateText(post.selftext, 100)}</p>
+            <a href="${post.url}" target="_blank" class="btn btn-primary">Read More</a>
+            <hr>
+            <span class="badge badge-secondary">Subreddit: ${post.subreddit}</span>
+            <span class="badge badge-dark">Score: ${post.score}</span>
+            </div>
+        </div>
+        `;
+    });
+    output += '<div>';
+    document.getElementById('results').innerHTML = output;
+  });
+
+  event.preventDefault();
+});
+
+function showMessage(message, className) {
+
+  const div = document.createElement('div');
+  div.className = `alert ${className}`;
+  div.appendChild(document.createTextNode(message));
+
+  const searchContainer = document.getElementById('search-container');
+  const search = document.getElementById('search');
+  searchContainer.insertBefore(div, search);
+
+  setTimeout(() => {
+    document.querySelector('.alert').remove();
+  }, 3000);
+}
+
+function truncateText(text, limit) {
+  const shortened = text.indexOf(' ', limit);
+  if (shortened == -1) return text;
+  return text.substring(0, shortened);
+}
+},{"./reddit.api":3}],4:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -92,7 +171,7 @@ module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
-  var ws = new WebSocket('ws://' + hostname + ':' + '53307' + '/');
+  var ws = new WebSocket('ws://' + hostname + ':' + '56351' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -193,5 +272,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}]},{},[6,4])
+},{}]},{},[4,2])
 //# sourceMappingURL=/dist/reddit-search-app.map
